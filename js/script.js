feeed042590e1b071ad3d366ce7bcb884b797f91
Dana <danacarroll2000@gmail.com>
2022-05-09 21:05:56 -0400

    Generated tasks now add to list

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const myKey = config.SECRET_KEY;
+let moodInputBox = document.getElementById('moodInput');
+const submitButton = document.getElementById('submitButton');
+const feelingList = document.getElementById('feelingList');
+let taskArray = [];
+localStorage.setItem('allTasks', JSON.stringify(taskArray));
+const taskData = JSON.parse(localStorage.getItem('allTasks'));
 
-let moodInput = 'Angry';
 
 let data = {
-    prompt: `Suggest one positive thing I should do based on my mood.\n Mood:${moodInput}\n`,
+    prompt: `Suggest one positive thing I can do based on my mood.\n Mood:${moodInput}`,
     temperature: 0.9,
-    max_tokens: 20,
+    max_tokens: 40,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
@@ -24,7 +29,43 @@ async function getResponse() {
         body: JSON.stringify(data),
         })
     const answer = await response.json();
-    console.log(answer.choices[0].text);
+    return answer.choices[0].text;
 }
 
-getResponse();
+function createTask(mood, task) {
+    return `
+        <div class="feelingThumbnail genResponseBK">
+            <h2 class="genEmotion">${mood}</h2>
+            <p class="genTask">${task}</p>
+        </div>
+    `;
+}
+
+submitButton.addEventListener('click', () => {
+    let moodInput = moodInputBox.value;
+    let moodTask = {
+        mood: '',
+        task: ''
+    }
+    data.prompt =  `Suggest one positive thing I can do based on my mood.\n Mood:${moodInput}\n`;
+    getResponse().then(text => {
+        let newText = text.slice(1);
+        moodTask.task = newText;
+        moodTask.mood = moodInput;
+        taskArray.push(moodTask);
+    }).then(() => {
+        console.log(taskArray);
+        localStorage.setItem('allTasks', JSON.stringify(taskArray));
+        let taskHTML = taskArray.map(function(element) {
+            return `
+                <div class="feelingThumbnail genResponseBK">
+                    <h2 class="genEmotion">${element.mood}</h2>
+                    <p class="genTask">${element.task}</p>
+                </div>
+            `;
+        })
+        taskHTML = taskHTML.join('');
+        console.log(taskHTML);
+        feelingList.innerHTML = taskHTML;
+    });
+})
