7d4846395a9f4f2626f8d9165568d15603cfcefc
Dana <danacarroll2000@gmail.com>
2022-05-11 18:53:30 -0400

    Made main element height dynamic

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 const menuButton = document.getElementById('menuButton');
 const exitMobileMenu = document.getElementById('exitMobileMenu');
 const mobileNav = document.getElementById('mobileNav');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
 
 
 menuButton.addEventListener('click', () => {
@@ -11,3 +14,9 @@ menuButton.addEventListener('click', () => {
 exitMobileMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('activeMobileNav');
 })
+
+const bodyHeight = body.clientHeight;
+const headerHeight = header.clientHeight;
+const windowHeight = window.innerHeight;
+
+main.style.height = `${bodyHeight - headerHeight}px`;
