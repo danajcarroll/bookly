ad9c8882670203bc6877e156bf02bee38e314ea6
Dana <danacarroll2000@gmail.com>
2022-05-14 09:58:49 -0400

    Animated vent content on screen

--- js/vent.js
+++ js/vent.js
@@ -7,3 +7,32 @@ trashButton.addEventListener('click', () => {
     // Animate the ventInput.value off screen somehow
     ventInput.value = '';
 })
+
+
+let ventAnimation = gsap.timeline();
+
+ventAnimation.from('.titleHeader', {
+    x: -100,
+    opacity: 0,
+    duration: 0.5
+}, 0.5)
+.from('.titleSub', {
+    x: -100,
+    opacity: 0,
+    duration: 0.5
+}, 0.75)
+.from('.ventInputBox', {
+    // height: 0,
+    opacity: 0,
+    duration: 0.5
+}, 1.25)
+.from('.trashButton', {
+    // width: 0,
+    opacity: 0,
+    duration: 0.5
+}, 1.25)
+.from('.inputText', {
+    // width: 0,
+    opacity: 0,
+    duration: 0.5
+}, 1.25)
