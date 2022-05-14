8a814f1288b29ca04c3839308710f6f5a006830e
Dana <danacarroll2000@gmail.com>
2022-05-14 09:47:41 -0400

    Added delay to mobile nav page change

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ const mobileNav = document.getElementById('mobileNav');
 const body = document.querySelector('body');
 const main = document.querySelector('main');
 const header = document.querySelector('header');
+const navButtonList = document.getElementsByClassName('mobNavListItem');
+const navButtons = [...navButtonList];
+const bodyHeight = body.clientHeight;
+const headerHeight = header.clientHeight;
+const windowHeight = window.innerHeight;
 
 
 menuButton.addEventListener('click', () => {
@@ -15,37 +20,46 @@ menuButton.addEventListener('click', () => {
 exitMobileMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('activeMobileNav');
 })
+navButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        mobileNav.classList.remove('activeMobileNav');
 
-const bodyHeight = body.clientHeight;
-const headerHeight = header.clientHeight;
-const windowHeight = window.innerHeight;
-
+        setTimeout(function () {
+            window.location.href = button.href;
+        }, 1000)
+    })
+});
 main.style.height = `${bodyHeight - headerHeight}px`;
 
+
+
 let homeAnimation = gsap.timeline();
+
+homeAnimation.from('.logo', {
+    y: -100,
+    duration: 0.5
+}, 0)
+.from('.instructions', {
+    y: -50,
+    opacity: 0,
+    duration: 0.5
+}, 0.25)
+.from('.moodInput', {
+    y: -50,
+    opacity: 0,
+    duration: 0.5
+}, 0.5)
+.from('.submitButton', {
+    y: -50,
+    opacity: 0,
+    duration: 0.5
+}, 0.5)
 
 window.addEventListener('DOMContentLoaded', () => {
     // setTimeout(() => {
     //     mobileNav.classList.remove('activeMobileNav');
     // }, 500);
-    homeAnimation.from('.logo', {
-        y: -100,
-        duration: 0.5
-    }, 0)
-    .from('.instructions', {
-        y: -50,
-        opacity: 0,
-        duration: 0.5
-    }, 0.25)
-    .from('.moodInput', {
-        y: -50,
-        opacity: 0,
-        duration: 0.5
-    }, 0.5)
-    .from('.submitButton', {
-        y: -50,
-        opacity: 0,
-        duration: 0.5
-    }, 0.5)
+
 
 })
