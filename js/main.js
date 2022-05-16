7be9bdcd28b54e1f73193b04dd2bcd7d42477c4d
Dana <danacarroll2000@gmail.com>
2022-05-15 20:17:59 -0400

    Removed comments

--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ const trashButton = document.getElementById('trashButton');
 const ventInput = document.getElementById('ventInputBox');
 
 trashButton.addEventListener('click', () => {
-    // Animate the ventInput.value off screen somehow
     ventInput.value = '';
 })
 
