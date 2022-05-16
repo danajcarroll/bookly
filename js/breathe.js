63c94416b6c9f81376b5dcb4fbce7a4b2f27f06a
Dana <danacarroll2000@gmail.com>
2022-05-16 13:02:59 -0400

    Setting up svg animation"

--- js/breathe.js
+++ js/breathe.js
@@ -4,46 +4,61 @@
 const breatheButtonList = document.querySelectorAll('.buttonBox button');
 const breatheButtons = [...breatheButtonList];
 
-
 let myObj = document.getElementById('obj');
 
-myObj.addEventListener('load', () => {
-    let mainCircle = myObj.getElementById('mainCircle');
-    let circ1 = myObj.getElementById('circ1');
-    let circ2 = myObj.getElementById('circ2');
-    let circ3 = myObj.getElementById('circ3');
-    let circ4 = myObj.getElementById('circ4');
-    let circ5 = myObj.getElementById('circ5');
-    let circ6 = myObj.getElementById('circ6');
-    let circ7 = myObj.getElementById('circ7');
-    let circ8 = myObj.getElementById('circ8');
-    
-    
-})
+// CREATING BREATHE ANIMATION
 
-let animationSetup = gsap.timeline();
+let miniCircles = myObj.getElementsByClassName('miniCircle');
 
-animationSetup.to('.animationSetup', {
-    y: 200,
-    duration: 1,
-    opacity: 0,
-    display: 'none'
-}, 2)
-.to('.animation', {
-    opacity: 1,
-    display: 'block',
-    duration: 1
-}, 4);
+    let breatheAnimation = gsap.timeline({duration: 2});
+    breatheAnimation.to(miniCircles, {
+        autoAlpha: 1,
+        stagger: {
+            from: 0,
+        }
+    })
+
+
+        // myObj.addEventListener('load', () => {
+        //     let mainCircle = myObj.getElementById('mainCircle');
+        //     let miniCircles = myObj.getElementsByClassName('miniCircle')
+            
+            
+        //     let breatheAnimation = gsap.timeline({duration: 2});
+        //     breatheAnimation.to(miniCircles, {
+        //         autoAlpha: 1,
+        //         stagger: {
+        //             from: 0,
+        //         }
+        //     })
+        // })
 
 
 breatheButtons.forEach(button => {
+    let animationSetup = gsap.timeline({paused: true});
+    animationSetup.to('.animationSetup', {
+        y: 200,
+        duration: 1,
+        opacity: 0,
+        display: 'none'
+    }, 2)
+    .to('.animation', {
+        opacity: 1,
+        display: 'block',
+        duration: 1
+    }, 4);
+
     let breathSelect = 0;
     button.addEventListener('click', () => {
         breathSelect = button.value;
-        animationSetup();
+        animationSetup.play();
+        // breatheAnimation();
 
 
+
+
+
+        
     })
-    
 });
 
