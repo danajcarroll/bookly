50ea4fcefaf22dc43e0d5a80fdc42a96f4475c99
Dana <danacarroll2000@gmail.com>
2022-05-17 14:42:19 -0400

    Breathe animation now up and running

--- js/breathe.js
+++ js/breathe.js
@@ -1,5 +1,5 @@
 'use strict';
-
+console.clear();
 // Breathe Page
 const breatheButtonList = document.querySelectorAll('.buttonBox button');
 const breatheButtons = [...breatheButtonList];
@@ -7,82 +7,100 @@ const breatheButtons = [...breatheButtonList];
 let miniCircles = document.getElementsByClassName('miniCircle');
 let breathSelect = 0;
 
-// CREATING BREATHE ANIMATION
-/* 
-button determines how many repeat
-yoyo makes animation go back and fourth
-
-*/
-
 gsap.set('.innerCircle', {
     scale: 0
 })
 gsap.set(miniCircles, {
     scale: 0
 })
-    let breatheAnimation = gsap.timeline({
-        paused: true,
-        // repeat: 1,
-        delay: 4,
-        ease: 'power1.out'
-    });
-    breatheAnimation.to('.innerCircle', {
-        autoAlpha: 1,
-        scale: 1,
-        duration: 5
-    }, 0)
-    .to(miniCircles, {
-        autoAlpha: 1,
-        scale: 1,
-        duration: 5,
-        stagger: {
-            each: 0.1,
-            from: 'start'
-        }
-    }, 0)
-    .to('.innerCircle', {
-        scale: 0,
-        duration: 5
-    }, 5)
-    .to(miniCircles, {
-        scale: 0,
-        duration: 5,
-        stagger: {
-            each: 0.1,
-            from: 'end'
-        }
-    }, 5);
-    // .to('.innerCircle', {
-    //     scale: 1,
-    //     duration: 3
-    // }, 3)
-    // .fromTo('.innerCircle', {
-    //     scale: 0
-    // }, {
-    //     scale: 1,
-    //     duration: 2,
-    //     yoyo: true,
-    //     repeat: 2
-    // }, 2);
 
-    let animationSetup = gsap.timeline({paused: true});
-    animationSetup.to('.animationSetup', {
-        y: 200,
-        duration: 1,
-        opacity: 0,
-        display: 'none'
+// First animation that will play
+// Hiding the "how many breaths" box and making div with animation inside appear
+let animationSetup = gsap.timeline({ paused: true });
+animationSetup
+.to(".animationSetup",
+    {
+    y: 200,
+    duration: 1,
+    opacity: 0,
+    display: "none"
     }, 1)
-    .to('.animation', {
-        opacity: 1,
-        display: 'block',
-        duration: 1
+.to(".animation",
+    {
+    opacity: 1,
+    display: "block",
+    duration: 1
     }, 2);
 
-breatheButtons.forEach(button => {
-    button.addEventListener('click', () => {
+// Second animation to play
+// The actual breathing animation that will repeat depending on user input
+let breatheAnimation = gsap.timeline({
+paused: true,
+delay: 4,
+ease: "power1.out",
+onComplete: () => {
+    console.warn("animationRestart");
+    animationRestart.restart()}
+});
+breatheAnimation
+.to(".innerCircle",
+    {
+    autoAlpha: 1,
+    scale: 1,
+    duration: 4
+    }, 0)
+.to(".innerCircle",
+    {
+    scale: 0,
+    duration: 4
+    }, 4)
+.to(miniCircles,
+    {
+    autoAlpha: 1,
+    scale: 1,
+    duration: 4,
+    stagger: {
+        each: 0.1,
+        from: "start"
+    }
+    }, 0)
+.to(miniCircles,
+    {
+    scale: 0,
+    duration: 4,
+    stagger: {
+        each: 0.1,
+        from: "end"
+    }
+    }, 4);
+
+// Third animation to play
+// Restarting the page, hiding the animation svg and making question appear again
+let animationRestart = gsap.timeline({
+paused: true,
+delay: 5
+});
+animationRestart
+.to(".animation",
+    {
+    duration: 1,
+    opacity: 0,
+    display: "none"
+    }, 0)
+.to(".animationSetup",
+    {
+    y: 0,
+    opacity: 1,
+    display: "flex",
+    duration: 1
+    }, 2);
+
+// Triggered by the button press
+breatheButtons.forEach((button) => {
+    button.addEventListener("click", () => {
         breathSelect = parseInt(button.value);
-        animationSetup.play();
-        breatheAnimation.play().repeat(breathSelect);
-    })
+        animationSetup.restart();
+        breatheAnimation.restart({delay: 2}).repeat(breathSelect);
+});
 });
 
